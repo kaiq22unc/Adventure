8f8ea19062f2e0fcd7483f1a117efa4192e290a2
kaiq22unc <sqin@unc.edu>
2022-09-26 00:47:35 -0400

    jedi

--- src/main/java/com/comp301/a02adventure/MapImpl.java
+++ src/main/java/com/comp301/a02adventure/MapImpl.java
@@ -12,7 +12,7 @@ public class MapImpl implements Map{
         this.height = height;
         this.numItems = numItems;
         this.grid = new Cell[width][height];
-        if(width <= 0 || height <= 0){
+        if(width <= 0 && height <= 0){
             throw new IllegalArgumentException();
         }
     }
