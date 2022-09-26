dc5c3de73d88715a8f9c9fa6b72366785f95d8ee
kaiq22unc <sqin@unc.edu>
2022-09-25 22:25:55 -0400

    novice

---
+++ src/main/java/com/comp301/a02adventure/GameImpl.java
@@ -0,0 +1,73 @@
+package com.comp301.a02adventure;
+
+import java.util.List;
+
+public class GameImpl implements Game{
+
+    private Map map;
+    private Player player;
+
+    public GameImpl(Map map, Player player) {
+        this.map = map;
+        this.player = player;
+        if(map == null || player == null){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Position getPlayerPosition() {
+        return player.getPosition();
+    }
+
+    public String getPlayerName() {
+        return player.getName();
+    }
+
+    public List<Item> getPlayerItems() {
+        return player.getInventory().getItems();
+    }
+
+    public boolean getIsWinner() {
+        return player.getInventory().getItems().size() == map.getNumItems();
+    }
+
+    public void printCellInfo() {
+        Cell c = map.getCell(player.getPosition());
+        System.out.println("Location: " + c.getName());
+        System.out.println(c.getDescription());
+        if(c.getIsVisited()){
+            System.out.println("You have already visited this location.");
+        }
+        if(c.hasChest()){
+            System.out.println("You found a chest! Type 'open' to see what's inside, or keep moving.");
+        }
+        c.visit();
+    }
+
+    public void openChest() {
+        Cell c = map.getCell(player.getPosition());
+        if(!c.hasChest()){
+            System.out.println("No chest to open, sorry!");
+        }else{
+            if(c.getChest() == null){
+                System.out.println("The chest is empty.");
+            }else{
+                player.getInventory().transferFrom(c.getChest());
+                System.out.println("You collected these items: " + c.getChest().getItems());
+            }
+        }
+    }
+
+    public boolean canMove(Direction direction) {
+        return player.getPosition().getNeighbor(direction) != null;
+    }
+
+    public void move(Direction direction) {
+        if(!this.canMove(direction)){
+            System.out.println("You can't go that way! Try another direction.");
+        }else{
+            player.move(direction);
+            this.printCellInfo();
+        }
+    }
+}
---
+++ src/main/java/com/comp301/a02adventure/MapImpl.java
@@ -0,0 +1,46 @@
+package com.comp301.a02adventure;
+
+public class MapImpl implements Map{
+
+    private Cell[][] grid;
+    private int width;
+    private int height;
+    private int numItems;
+    public MapImpl(int width, int height, int numItems){
+        this.width = width;
+        this.height = height;
+        this.numItems = numItems;
+        this.grid = new Cell[width][height];
+        if(width <= 0 || height <= 0){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Cell getCell(int x, int y) {
+        return grid[x][y];
+    }
+
+    public Cell getCell(Position position) {
+        return grid[position.getX()][position.getY()];
+    }
+
+    public void initCell(int x, int y) {
+        if(x>width || y>height){
+            throw new IndexOutOfBoundsException();
+        }else{
+            grid[x][y] = new CellImpl(x,y);
+        }
+    }
+
+    public int getNumItems() {
+        return numItems;
+    }
+}
